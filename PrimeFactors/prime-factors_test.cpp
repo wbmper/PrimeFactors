f56def30c0db743b7ee7936fa10fd7312e26e56f
yuz010 <rladbwmd123@gmail.com>
2025-03-27 14:30:24 +0900

    [feature] 33

--- PrimeFactors/prime-factors_test.cpp
+++ PrimeFactors/prime-factors_test.cpp
@@ -1,4 +1,14 @@
 #include "gmock/gmock.h"
+#include "prime-factors.cpp"
+
+#include <vector>
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
 
 int main() {
 	::testing::InitGoogleMock();
